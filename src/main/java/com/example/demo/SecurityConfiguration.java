667d2bec43af1e2390f5b95fe08fdeab7d898500
GBTC441012UR <GBTC441012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-08-13 15:41:12 -0400

    Initial

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public static PasswordEncoder encoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                .antMatchers("/admin").access("hasAnyAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
+                .and().httpBasic();
+    }
+    @Override
+    protected void  configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication().
+                withUser("Shabnam").password(encoder().encode("Shabnam12345")).authorities("ADMIN").and()
+                .withUser("user").password(encoder().encode("password")).authorities("USER")
+        .and().passwordEncoder(encoder());
+    }
+
+
+
+}
